d1bd08f3f96687b1bdcd37cd80f00e1a127f6a35
soni7431 <soni7431@gmail.com>
2021-01-18 01:11:32 +0530

    new work saved

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def test(req):
+    return HttpResponse("Hello World!")
+
+def displayname(req):
+    return HttpResponse("Hello satyesh!!!!")    
