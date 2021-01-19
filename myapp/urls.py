9502e9e1ad22ed2c1148ce5c5895eb4ee25a8e0a
soni7431 <soni7431@gmail.com>
2021-01-19 13:30:36 +0530

    static file and menu saved

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from .views import *
 urlpatterns = [
    path("",test),
-   path("myname",displayname)
+   path("paramfun/<str:pk>/<str:sk>",ParamFunction),
+   path("home/",HomePage, name='homepage'),
+   path("about/",Aboutus, name='aboutuspage'),
+   path("products/",Products, name='productpage'),
+   path("contact/",Contacts, name='contactpage')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -5,5 +5,21 @@ from django.http import HttpResponse
 def test(req):
     return HttpResponse("Hello World!")
 
-def displayname(req):
-    return HttpResponse("Hello satyesh!!!!")    
+def ParamFunction(req,sk,pk):
+    context={
+        'name' : pk,
+        'contact': sk
+    }    
+    return render(req,'param.html',context)
+
+def HomePage(req):
+    return render(req,"index.html", {'name':'smsoni infosoft'})    
+
+def Aboutus(req):
+    return render(req,"aboutus.html", {'contact':8128992581})  
+
+def Products(req):
+    return render(req,'products.html',{'productname':'C Book'})      
+
+def Contacts(req):
+    return render(req,'contactus.html',{'email':'soni7431@gmail.com'})    
